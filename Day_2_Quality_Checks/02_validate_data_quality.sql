9e2cfb1e75c44b359ad91a421a9a66c6fe4aad7e
Konduru Sri Vaishnavi <srivaishnavikonduru@gmail.com>
2026-09-18 02:10:05 +0530

    add day 2 data quality checks

---
+++ Day_2_Quality_Checks/02_validate_data_quality.sql
@@ -0,0 +1,42 @@
+-- Check for invalid or unexpected state and category values
+s
+SELECT DISTINCT state_id, cat_id
+FROM `fresh-yen-508710-a0.m5_raw.sales_train_validation`
+WHERE state_id NOT IN ('CA', 'TX', 'WI')
+   OR cat_id NOT IN ('FOODS', 'HOBBIES', 'HOUSEHOLD');
+
+-- Check for missing or invalid dates
+
+SELECT COUNT(*) AS invalid_date_count
+FROM `fresh-yen-508710-a0.m5_raw.Calendar`
+WHERE date IS NULL;
+
+-- Check the date range
+
+SELECT MIN(date) AS min_date, MAX(date) AS max_date
+FROM `fresh-yen-508710-a0.m5_raw.Calendar`;
+
+-- Check for negative sales values
+
+SELECT COUNT(*) AS negative_sales_count
+FROM `fresh-yen-508710-a0.m5_raw.sales_train_validation`
+WHERE d_1 < 0
+   OR d_2 < 0
+   OR d_3 < 0;
+
+--check for invalid sell_price values (negative or zero)
+
+SELECT COUNT(*) as invalid_price_count
+FROM `fresh-yen-508710-a0.m5_raw.sell_prices`
+WHERE sell_price <= 0;
+
+ -- Check for duplicate item, store, and week combinations
+
+SELECT
+  item_id,
+  store_id,
+  wm_yr_wk,
+  COUNT(*) AS duplicate_count
+FROM `fresh-yen-508710-a0.m5_raw.sell_prices`
+GROUP BY item_id, store_id, wm_yr_wk
+HAVING COUNT(*) > 1;
